d3f6b663a112513f0d0618848da6855e346cd6a4
Karol Kobiela <kobiaszu@gmail.com>
2017-09-03 16:00:16 +0200

    fixed task 3

---
+++ ConsoleApplication1/authorisation.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class authorisation
+    {
+        private string Username;
+        private string Password;
+        private int MaxTries;
+
+
+        public authorisation()
+        {
+            Username = "admin";
+            Password = "123";
+            MaxTries = 5;
+        }
+
+        private bool compare ( string username, string password)
+        {
+
+            if (username.Equals(Username) && password.Equals(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private string getString( string type)
+        {
+            string tray = "";
+            bool check = false;
+            do
+            {
+                if (check)
+                    Console.WriteLine($"Pole {type} nie może być puste, spróbuj jeszcze raz: ");
+                else
+                    Console.WriteLine($"Podaj {type}:");
+                tray = Console.ReadLine();
+                check = true;
+            } while (tray == "");
+
+            return tray;
+        } 
+
+        public void login()
+        {
+            int counter = 0;
+            bool result = false;
+            do
+            {
+                result = compare(getString("uzytkownik"), getString("haslo"));
+                if (result)
+                    break;
+                else
+                    Console.WriteLine("Nie udalo sie, sprobuj jeszcze raz!");
+                counter++;
+            }
+            while (counter <= MaxTries);
+
+            if (result)
+            {
+                Console.WriteLine($"Zalogowano po {counter + 1} probach");
+            }
+            else
+            {
+                Console.WriteLine($"Przekroczono licznik {MaxTries} prob!");
+            }
+
+            
+        }
+
+    }
+}
---
+++ ConsoleApplication1/funkcyje.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class funkcyje
+    {
+        public static void AddRandomNumbers(object min, object max)
+        {
+            Random rnd = new Random();
+            int p1 = 0, p2 = 0;
+            if (min is int)
+            {
+                p1 = rnd.Next((int)min, (int)max);
+                p2 = rnd.Next((int)min, (int)max);
+            }
+            else
+            {
+                Console.WriteLine("dupa nie dziala");
+            }
+
+
+            
+
+            Console.WriteLine($"wylosowano: {p1} i {p2}, suma wynosi {p1 + p2}");
+
+            Console.ReadKey();
+        }
+
+
+    }
+}
