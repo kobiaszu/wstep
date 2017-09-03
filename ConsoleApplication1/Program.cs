3315f0ca6b458bccf437eb5795dd5592a880712f
Karol Kobiela <kobiaszu@gmail.com>
2017-09-03 13:58:30 +0200

    zrobione

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -8,8 +8,60 @@ namespace ConsoleApplication1
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
+
+
+            Console.WriteLine("Siema, podaj swoje imie");
+            string name = "";
+            while (name == "")
+            {
+                name = Console.ReadLine();
+            }
+
+
+            int age = 0;
+            do
+            {
+                Console.WriteLine("Podaj wiek: ");
+                Int32.TryParse(Console.ReadLine(), out age);
+            } while (age <= 0);
+
+            Console.WriteLine($"Jestes {name} i masz {age} lat");
+
+
+            Console.WriteLine("Tera losowanie");
+
+
+            Random rnd = new Random();
+
+            int max = sizeof(int), min = sizeof(int);
+
+            bool check = false;
+            do
+            {
+                Console.WriteLine("Podaj min: ");
+                check = Int32.TryParse(Console.ReadLine(), out min);
+            } while (min == sizeof(int) || !check);
+           
+            do
+            {
+                Console.WriteLine("Podaj max: ");
+                check = Int32.TryParse(Console.ReadLine(), out max);
+            } while (max == sizeof(int) || (max <= min) || !check);
+        
+            int p1 = rnd.Next(min, max);
+            int p2 = rnd.Next(min, max);
+
+            Console.WriteLine($"wylosowano: {p1} i {p2}, suma wynosi {p1 + p2}");
+
+            Console.ReadKey();
+
+
+
+            
         }
     }
 }
