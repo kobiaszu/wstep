b248cde6f675f3d62dbdf716a6eefe987df4ff2b
Karol Kobiela <kobiaszu@gmail.com>
2017-09-10 22:14:50 +0200

    Extension example

---
+++ ConsoleApplication1/ExtensionExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public static class ExtensionExample
+    {
+        public static Dictionary<string, bool> blackDictonary = new Dictionary<string, bool>()
+        {
+            { "dupa", true },
+            { "dupa1", true },
+            { "dupa2", true },
+            { "dupa3", true },
+            { "dupa4", false }
+        };
+
+        public static List<string> blackList = new List<string>()
+        {
+             "dupa",
+             "dupa1",
+             "dupa2",
+             "dupa3",
+             "dupa4"
+        };
+
+
+        public static bool CheckIfBlackDictonary(this string s)
+        {
+            try
+            {
+                return blackDictonary[s];
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool CheckIfBlackList(this string s)
+        {
+            var result = blackList.FirstOrDefault(x => x.Contains(s));
+            Console.WriteLine($"ODPALAM CHECK: {result}");
+
+            return result != null ? true : false;
+        }
+
+
+    }
+}
--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,10 +10,66 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-   
-            Authorisation logowanie = new Authorisation("karol", "321", 3);
-            Authorisation logowanie1 = new Authorisation();
-            logowanie.login();
+
+            //Authorisation logowanie = new Authorisation("karol", "321", 3);
+            //Authorisation logowanie1 = new Authorisation();
+            //logowanie.login();
+
+            string check = "dupen";
+            string result = "";
+
+            if (ExtensionExample.CheckIfBlackDictonary(check))
+            {
+                result = "jest na slowniku :)";
+            }
+            else
+            {
+                result = "nie ma na slowniku :(";
+            }
+
+            Console.WriteLine($"Result for 'dupen': {result}");
+
+            ExtensionExample.blackDictonary.Add(check, true);
+
+            if (ExtensionExample.CheckIfBlackDictonary(check))
+            {
+                result = "jest na slowniku :)";
+            }
+            else
+            {
+                result = "nie ma na slowniku :(";
+            }
+
+            Console.WriteLine($"Result for 'dupen': {result}");
+
+            // TERA LISTA
+
+
+
+            if (ExtensionExample.CheckIfBlackList(check))
+            {
+                result = "jest na liscie :)";
+            }
+            else
+            {
+                result = "nie ma na liście :(";
+            }
+
+            Console.WriteLine($"Result for 'dupen': {result}");
+
+            ExtensionExample.blackList.Add(check);
+
+            if (ExtensionExample.CheckIfBlackList(check))
+            {
+                result = "jest na liscie :)";
+            }
+            else
+            {
+                result = "nie ma na liście :(";
+            }
+
+            Console.WriteLine($"Result for 'dupen': {result}");
+
 
             Console.ReadKey();
 
